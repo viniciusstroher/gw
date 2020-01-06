a79d4c84a4d4a9159b8f59069b0fb03035cd5dcc
Stroher <vinicius.stroher@rcxit.com.br>
2020-01-06 08:23:31 -0300

    fix

---
+++ cli/deleteUser.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."includes.dependencies.php";
+
+
+try{
+	
+	$username = $argv[1];
+	
+	if(empty($username)){
+		throw new Exception("Parametro username nao pode ser vazio", 1);
+	}
+
+	$envVars = Utils::getEnvVars(Utils::$logPath);
+	$db = new PgSql($envVars['host'],
+					$envVars['port'],
+					$envVars['db'],
+					$envVars['username'],
+					$envVars['password']);
+
+	$sql = "delete from users where username = '$username'";
+	$db->exec($sql);
+
+	print "User $username deleted\n";
+
+}catch(Exception $e){
+	
+	Utils::log($e->getMessage());
+	print $e->getMessage()."\n";
+
+}finally{
+	$db->close();
+}
